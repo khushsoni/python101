f38713bc9a0b07c0daf447f00c902aa65bc6bb61
Khush Soni <81143064+khushsoni@users.noreply.github.com>
2024-01-04 22:10:15 +0530

    Created oneshot file from yt

---
+++ oneshot.py
@@ -0,0 +1,45 @@
+# https://youtu.be/ihk_Xglr164
+# Python Tutorial in Hindi | Code with Harry
+
+# import cv2
+# import math
+
+
+# print(math.gcd(3,6))
+# print("Hello world")
+
+# a = 34
+# b = "Harry"
+# c = 45.32
+
+#print(a+c)
+
+#print(type(a))
+
+# e = "31"
+# e = int(e) # type casting
+
+# print(type(e))
+# print(e+2)
+
+# name = '''my name is
+#         Harry'''
+        
+# print(name)
+
+# name = "   Khush  "
+# print(name[2:4])
+# print(name.strip())
+
+# name = "Khush"
+# print(len(name))
+
+# str1 = "This is a "
+# name = "Khush"
+# print(str1 + name)
+
+name1 = "Khush"
+name2 = "Harry"
+#temp = "This is a {1} and he is a good boy named {0}".format(name1,name2)
+temp2 = f"This is a {name1} and he is a good boy named {name2}"
+print(temp2)
