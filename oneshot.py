11bddd845ca7715713e1ada57641417921872b11
Khush Soni <81143064+khushsoni@users.noreply.github.com>
2024-01-05 21:56:34 +0530

    Update oneshot.py

--- oneshot.py
+++ oneshot.py
@@ -38,8 +38,31 @@
 # name = "Khush"
 # print(str1 + name)
 
-name1 = "Khush"
-name2 = "Harry"
-#temp = "This is a {1} and he is a good boy named {0}".format(name1,name2)
-temp2 = f"This is a {name1} and he is a good boy named {name2}"
-print(temp2)
+# name1 = "Khush"
+# name2 = "Harry"
+# #temp = "This is a {1} and he is a good boy named {0}".format(name1,name2)
+# temp2 = f"This is a {name1} and he is a good boy named {name2}"
+# print(temp2)
+
+# operators:
+# ** operator
+#print(2**4)
+
+# // Floor division 
+# print(5//2) # 2
+
+# % Modulo operator (remainder - shesh in gujarati)
+# print(5 % 2)
+
+# list 
+lst = [2, 14, 45, 61, 57]
+lst.append(100)
+lst.insert(1,100)
+lst.remove(61)
+lst.pop()
+del lst[3]
+#del lst
+lst.clear()
+var = lst
+
+print(var)
